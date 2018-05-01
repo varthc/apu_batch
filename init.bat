331093cceae7900c30276a3d84c81f950a96b701
C <32149970+varthc@users.noreply.github.com>
2018-05-01 14:05:16 -0300

    Añede parametro para que sea opcional la creacion del archivo exclude.

--- init.bat
+++ init.bat
@@ -1,23 +1,29 @@
 @echo off
 
-::Obtiene el path de donde esta el archivo.
+rem Obtiene parametro para crear o no el archivo exclude.
+set createExcludeFile=%~1
+
+rem Obtiene el path de donde esta el archivo.
 set projectPath="%~dp0"
 
-::Copia proxy.
+rem Copia proxy.
 @ xcopy /s /i /Y "%projectPath%\files\proxy.conf.json" "%projectPath%" >NUL
 echo Copiado proxy.
 
-::Copia auth service.
+rem Copia auth service.
 @ xcopy /s /i /Y "%projectPath%\files\authorization.service.ts" "%projectPath%\src\app" >NUL
 echo Copiado auth service.
 
 ::Crea archivo exclude y agrega lo que quiero excluir localmente.
-(
-echo #Files and folders to ignore
-echo /files
-echo .vscode/
-echo init.bat
-) > "%projectPath%\.git\info\exclude"
+if "%createExcludeFile%" == "1" (
+	(
+		echo #Files and folders to ignore
+		echo /files
+		echo .vscode/
+		echo init.bat
+	) > "%projectPath%\.git\info\exclude"
 
-echo Creado ignore local.
+	echo Creado ignore local.
+)
+
 echo Gracias, vuelva prontos!
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 Mini batch para evitar el insufrible copy/paste diario.
 
+cmd init [1]
 
+[1] Opcional - crea el archivo local de ignore.
